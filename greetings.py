e438787aeccc392bbbd5d285db069bab26a59870
mugivara132 <mugivara132@icloud.com>
2024-12-17 16:47:19 +0500

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,12 @@
+username = 'Kirill '
+print (username  +  '- имя пользователя')
+title = 'Заметки по учебе '
+print (title + ' - заголовок заметки')
+content = 'Здесь будут размещены подсказки, которые помогут с освоением программы '
+print (content + ' - Описание заметки')
+status = 'В разработке '
+print (status + ' - Статус заметки')
+created_date = '17-12-2024 '
+print (created_date + ' - Дата создания заметки')
+issue_date = '19.12.2024 '
+print (issue_date + ' - дата истечения заметки')
