ac0a1e39f90a7597460bce0a5575ba5297431690
mugivara132 <mugivara132@icloud.com>
2025-01-13 20:21:00 +0500

    Add files via upload

--- date_changer.py
+++ date_changer.py
@@ -1,9 +1,18 @@
-created_date = '17-12-2024'
-issue_date = '19-12-2024'
+username = 'Kirill '
+print(username  +  '- имя пользователя')
+title = 'Заметки по учебе '
+print(title + ' - заголовок заметки')
+content = 'Здесь будут размещены подсказки, которые помогут с освоением программы '
+print(content + ' - Описание заметки')
+status = 'В разработке '
+print(status + ' - Статус заметки')
+created_date = '17-12-2024 '
+issue_date = '19-12-2024 '
 temp_created_date = created_date[0:5]
 temp_issue_date = issue_date [0:5]
-print (temp_created_date)
-print (temp_issue_date)
+print(temp_issue_date + ' - дата истечения заметки')
+print(temp_created_date + ' - Дата создания заметки')
+
 
 
 
